ae25ddf57c2c23db518514b2c951450d4334b746
Mahesh Ramachandran <mahesh.r@royalcyber.com>
2026-08-11 17:02:31 +0530

    refactor(webapp): colocate UI library

--- apps/webapp/tailwind.config.ts
+++ apps/webapp/tailwind.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "tailwindcss";
-import { meridianTailwindPreset } from "@csa/ui/preset";
+import { meridianTailwindPreset } from "./src/ui/preset";
 
 const config: Config = {
   presets: [meridianTailwindPreset as any],
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   plugins: []
 };
